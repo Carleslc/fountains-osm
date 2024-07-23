d886bea31639157ce35d0bd24067f0e79f9b48f4
Carleslc <lazaro.costa.carles@gmail.com>
2024-07-23 17:52:50 +0200

    Script update_fountains wait for container

--- update_fountains.sh
+++ update_fountains.sh
@@ -9,22 +9,32 @@ load_env() {
 load_env
 
 if [ -n "$PROVIDERS_CLI" ]; then
-  # Command for providers-cli
-  CMD_PROVIDERS="docker compose --env-file .env up -d providers-cli"
-
   # Run providers-cli
-  # echo $CMD_PROVIDERS
-  eval $CMD_PROVIDERS
+  docker compose --env-file .env up -d providers-cli
 
   # Check if providers-cli command was successful
   if [ $? -ne 0 ]; then
+    echo "Failed to start providers-cli"
+    exit 1
+  fi
+
+  # Wait for providers-cli to finish
+  PROVIDERS_CLI_CONTAINER_ID=$(docker compose --env-file .env ps -q providers-cli)
+  docker wait $PROVIDERS_CLI_CONTAINER_ID
+
+  # Check if providers-cli exited successfully
+  PROVIDERS_CLI_EXIT_CODE=$(docker inspect $PROVIDERS_CLI_CONTAINER_ID --format='{{.State.ExitCode}}')
+  if [ $PROVIDERS_CLI_EXIT_CODE -ne 0 ]; then
+    echo "providers-cli finished with errors"
     exit 1
   fi
 fi
 
-# Command for fountains-cli
-CMD_FOUNTAINS="docker compose --env-file .env up -d fountains-cli"
-
 # Run fountains-cli
-# echo $CMD_FOUNTAINS
-eval $CMD_FOUNTAINS
+docker compose --env-file .env up -d fountains-cli
+
+# Check if fountains-cli command was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to start fountains-cli"
+  exit 1
+fi
